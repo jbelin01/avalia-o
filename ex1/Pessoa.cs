1d58fa98f543532e3197fdbc888407007c253621
João Gabriel <joao_gabriel.silva15@hotmail.com>
2024-04-26 14:26:17 -0300

    troca de exemplo

---
+++ ex1/Pessoa.cs
@@ -0,0 +1,18 @@
+public class Pessoa{
+
+    public string nome;
+
+    public string raca; 
+
+    public int idade;
+
+    public Pessoa(string nome, string raça, int idade)
+    {
+        nome = nome;
+        raca = raca;
+        idade = idade;
+    }
+
+    
+    
+}
