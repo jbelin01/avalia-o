3f52c614b88ad93399e21bb5c6060cd73a8af840
João <joao_gabriel.silva15@hotmail.com>
2024-04-26 15:17:01 -0300

    ex1 feito

--- Program.cs
+++
@@ -1,2 +0,0 @@
-Garcom  = new Garcom();
-
---
+++ ex1/Program.cs
@@ -0,0 +1,28 @@
+Garcom garcom = new Garcom();
+
+garcom.nome = "Jerson";
+garcom.raca = "pardo";
+garcom.idade = 22;
+
+        Console.WriteLine("Garçom:");
+        Console.WriteLine("Nome: " + garcom.nome);
+        Console.WriteLine("Raça: " + garcom.raca);
+        Console.WriteLine("Idade: " + garcom.idade);
+        Console.WriteLine();
+
+Juiz juiz = new Juiz();
+
+juiz.nome = "Leoncio";
+juiz.raca = "Asiático";
+juiz.idade = 62;
+
+        Console.WriteLine("Juiz:");
+        Console.WriteLine("Nome: " + juiz.nome);
+        Console.WriteLine("Raça: " + juiz.raca);
+        Console.WriteLine("Idade: " + juiz.idade);
+        Console.WriteLine();
+
+
+
+
+
