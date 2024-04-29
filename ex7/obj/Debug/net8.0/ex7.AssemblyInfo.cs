a54e68a815c60308f0b93fae59496e094ee1e65b
João <joao_gabriel.silva15@hotmail.com>
2024-04-29 12:29:09 -0300

    pastas

--- ex7/obj/Debug/net8.0/ex7.AssemblyInfo.cs
+++ ex7/obj/Debug/net8.0/ex7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64b3b599cc11a48a20917c726925dc4b86f44068")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33f85e21a752b20214dcdbb8c3be275d7cc666a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
